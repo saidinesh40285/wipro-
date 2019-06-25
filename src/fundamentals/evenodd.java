c8f70ff79271f6f4eb92ad3268e32c50e135f80d
saidinesh40285 <52160201+saidinesh40285@users.noreply.github.com>
2019-06-25 11:24:06 +0530

    control statements

---
+++ src/fundamentals/evenodd.java
@@ -0,0 +1,16 @@
+package fundamentals;
+
+public class evenodd {
+	public static void main(String args[])
+	{
+		int s;
+		s=Integer.parseInt(args[0]);
+		if((s%2)==0)
+		{
+			System.out.println(" even ");
+		}
+		else
+			System.out.println(" odd ");
+	}
+
+}
