9133e08c6c3fa656406c5b6d9f92a0dae0863b3e
saidinesh40285 <52160201+saidinesh40285@users.noreply.github.com>
2019-06-25 11:17:54 +0530

    control statements1

---
+++ src/fundamentals/positivenegitive.java
@@ -0,0 +1,14 @@
+package fundamentals;
+
+public class positivenegitive {
+	public static void main(String args[]) {
+		int s;
+		s=Integer.parseInt(args[0]);
+		if(s<0)
+			System.out.println("Negitve");
+		else
+			System.out.println("Positve");
+			
+	}
+
+}
